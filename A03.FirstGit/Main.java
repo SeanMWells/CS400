755e0df97adbdeac92a71a4a9605cf0309a4cab0
Sean Wells <seanmwells34@gmail.com>
2024-10-04 17:56:46 -0400

    A03-05

---
+++ A03.FirstGit/Main.java
@@ -0,0 +1,11 @@
+/**
+* Main class for CS400 A03.FirstGit activity Spring - 2024
+* @author: Sean Wells, smwells3
+**/
+
+public class Main {
+    public static void main(String[] args) {
+	System.out.println("Why did two foos walks into a bar?");
+	System.out.println("To say \"Hello World\" to their friend Baz.");
+    }
+}
